066526c9dc49811c1d184f66b46fcf8e2b7367cb
Jean Silva <jeanbarrossilva@outlook.com>
2021-05-13 20:35:42 -0300

    Add Activity toolbar extension

--- extensions/src/main/java/com/jeanbarrossilva/period/extensions/activity/Activity.kt
+++ extensions/src/main/java/com/jeanbarrossilva/period/extensions/activity/Activity.kt
@@ -2,10 +2,16 @@ package com.jeanbarrossilva.period.extensions.activity
 
 import android.app.Activity
 import android.widget.FrameLayout
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.children
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.jeanbarrossilva.period.extensions.view.searchFor
 
 private val Activity.content
     get() = findViewById<FrameLayout>(android.R.id.content)
+
+val Activity.toolbar
+    get() = view.searchFor<Toolbar>() ?: view.searchFor<BottomAppBar>()
 
 val Activity.view
     get() = content.children.first()
