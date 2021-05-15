1427cc23c4df5fea7bd6a9e8fa361086d5094da4
Jean Silva <jeanbarrossilva@outlook.com>
2021-05-15 07:14:53 -0300

    Override toString() in ChemicalElementProperty

--- model/src/main/java/com/jeanbarrossilva/period/model/ChemicalElementProperty.kt
+++ model/src/main/java/com/jeanbarrossilva/period/model/ChemicalElementProperty.kt
@@ -4,5 +4,9 @@ import java.io.Serializable
 import kotlin.reflect.KProperty
 
 open class ChemicalElementProperty<T> internal constructor(open val value: T): Serializable {
+    override fun toString(): String {
+        return "ChemicalElementProperty(value=$value)"
+    }
+
     operator fun getValue(thisRef: Any?, property: KProperty<*>) = value
 }
