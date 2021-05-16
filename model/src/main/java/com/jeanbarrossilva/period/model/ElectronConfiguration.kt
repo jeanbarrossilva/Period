f88c2d73f28cdbbac1538b41c28d613476379ccd
Jean Silva <jeanbarrossilva@outlook.com>
2021-05-16 19:04:43 -0300

    Add option to sort by electron config

--- model/src/main/java/com/jeanbarrossilva/period/model/ElectronConfiguration.kt
+++ model/src/main/java/com/jeanbarrossilva/period/model/ElectronConfiguration.kt
@@ -1,12 +1,25 @@
 package com.jeanbarrossilva.period.model
 
-class ElectronConfiguration(vararg distributions: ElectronDistribution): ChemicalElementProperty<String>(distributions.joinToString(" ") { distribution ->
-    distribution.value
-}) {
+class ElectronConfiguration(private vararg val distributions: ElectronDistribution):
+    ChemicalElementProperty<String>(distributions.joinToString(" ") { distribution ->
+        distribution.value
+    }),
+    Comparable<ElectronConfiguration> {
     private val limit = ElectronDistribution::class.sealedSubclasses.size
 
     init {
         if (distributions.size > limit)
             error("Exceeded the size limit of $limit distributions!")
+    }
+
+    override fun compareTo(other: ElectronConfiguration): Int {
+        val electronCount = distributions.map { distribution ->
+            distribution.subshell.electronCount
+        }.sum()
+        val otherElectronCount = other.distributions.map { distribution ->
+            distribution.subshell.electronCount
+        }.sum()
+
+        return electronCount.compareTo(otherElectronCount)
     }
 }
