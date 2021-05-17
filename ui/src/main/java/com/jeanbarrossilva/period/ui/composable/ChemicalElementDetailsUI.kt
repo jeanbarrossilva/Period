53d9a0974fcad2e1d25ac787d9d1e9767e8163c2
Jean Silva <jeanbarrossilva@outlook.com>
2021-05-17 13:31:07 -0300

    Add landscape UI for element details

--- ui/src/main/java/com/jeanbarrossilva/period/ui/composable/ChemicalElementDetailsUI.kt
+++ ui/src/main/java/com/jeanbarrossilva/period/ui/composable/ChemicalElementDetailsUI.kt
@@ -1,91 +1,110 @@
 package com.jeanbarrossilva.period.ui.composable
 
-import android.view.ViewGroup
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ContentAlpha
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.view.setMargins
+import androidx.recyclerview.widget.ItemTouchHelper
 import com.jeanbarrossilva.period.extensions.number.dp
+import com.jeanbarrossilva.period.extensions.view.updatePadding
 import com.jeanbarrossilva.period.model.ChemicalElement
+import com.jeanbarrossilva.period.ui.composable.component.ChemicalElementHeadline
+import com.jeanbarrossilva.period.ui.composable.component.core.Landscape
+import com.jeanbarrossilva.period.ui.composable.component.core.Portrait
 import com.jeanbarrossilva.period.ui.composable.theme.PeriodTheme
-import com.jeanbarrossilva.period.ui.composable.theme.androidEuclidBold
-import com.jeanbarrossilva.period.ui.fragment.ChemicalElementDetailsFragment
 import com.jeanbarrossilva.period.ui.view.ChemicalElementDetailsView
-import com.jeanbarrossilva.period.ui.view.ChemicalElementSymbolView
 
 @Composable
-fun ChemicalElementDetailsFragment.ChemicalElementDetailsUI(element: ChemicalElement, modifier: Modifier = Modifier) {
-    PeriodTheme(modifier) {
-        LazyColumn {
-            item {
-                Column(
-                    Modifier
-                        .fillMaxWidth()
-                        .padding(top = 30.dp, bottom = 92.dp),
-                    Arrangement.spacedBy(50.dp),
-                    Alignment.CenterHorizontally
-                ) {
+fun ChemicalElementDetailsUI(element: ChemicalElement, modifier: Modifier = Modifier) {
+    PeriodTheme {
+        Portrait {
+            LazyColumn(
+                Modifier
+                    .fillMaxSize()
+            ) {
+                item {
                     Column(
-                        verticalArrangement = Arrangement.spacedBy(30.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
+                        modifier
+                            .fillMaxSize()
+                            .padding(top = 50.dp),
+                        Arrangement.spacedBy(50.dp)
                     ) {
-                        AndroidView(
-                            { context ->
-                                ChemicalElementSymbolView(context)
-                            },
+                        Box(
                             Modifier
-                                .shadow(elevation = 4.dp, RoundedCornerShape(20.dp))
-                        ) { view ->
-                            view.element = element
-                            view.layoutParams = ViewGroup.MarginLayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT).apply {
-                                setMargins(4.dp(context))
-                            }
+                                .fillMaxWidth(),
+                            Alignment.Center
+                        ) {
+                            ChemicalElementHeadline(element)
                         }
 
-                        Column(
-                            verticalArrangement = Arrangement.spacedBy(5.dp),
-                            horizontalAlignment = Alignment.CenterHorizontally
+                        Box(
+                            Modifier
+                                .fillMaxSize(),
+                            Alignment.Center
                         ) {
-                            Text(
-                                element.name.value,
-                                fontSize = 35.sp,
-                                fontFamily = androidEuclidBold,
-                                textAlign = TextAlign.Center
-                            )
-
-                            Text(
-                                element.electronConfiguration.value,
-                                Modifier.alpha(ContentAlpha.medium),
-                                fontSize = 14.sp,
-                                textAlign = TextAlign.Center
-                            )
+                            AndroidView(
+                                { context ->
+                                    ChemicalElementDetailsView(context)
+                                },
+                                Modifier
+                                    .fillMaxSize()
+                            ) { view ->
+                                view.element = element
+                                view.clipToPadding = false
+                                if (view.canScrollVertically(ItemTouchHelper.UP)) {
+                                    view.updatePadding(top = 20.dp(view.context), bottom = 20.dp(view.context))
+                                }
+                            }
                         }
                     }
                 }
+            }
+        }
 
-                AndroidView(
-                    { context ->
-                        ChemicalElementDetailsView(context)
-                    },
+        Landscape {
+            Row(
+                Modifier
+                    .fillMaxSize()
+            ) {
+                Box(
                     Modifier
-                        .fillMaxWidth()
-                        .padding(10.dp)
-                ) { view ->
-                    view.element = element
+                        .fillMaxWidth(0.5f)
+                        .fillMaxHeight()
+                        .padding(20.dp),
+                    Alignment.Center
+                ) {
+                    ChemicalElementHeadline(element)
+                }
+
+                Box(
+                    Modifier
+                        .fillMaxSize()
+                        .padding(20.dp),
+                    Alignment.Center
+                ) {
+                    LazyColumn(
+                        Modifier
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.spacedBy(0.dp, Alignment.CenterVertically)
+                    ) {
+                        item {
+                            AndroidView(
+                                { context ->
+                                    ChemicalElementDetailsView(context)
+                                },
+                                Modifier
+                                    .fillMaxSize()
+                            ) { view ->
+                                view.element = element
+                                view.clipToPadding = false
+                                if (view.canScrollVertically(ItemTouchHelper.UP))
+                                    view.updatePadding(top = 20.dp(view.context), bottom = 20.dp(view.context))
+                            }
+                        }
+                    }
                 }
             }
         }
