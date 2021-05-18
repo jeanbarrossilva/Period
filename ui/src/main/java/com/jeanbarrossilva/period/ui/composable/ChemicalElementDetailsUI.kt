7039f4dda2d47cd4e5ed0a804992d23bcd2f034d
Jean Silva <jeanbarrossilva@outlook.com>
2021-05-17 21:00:53 -0300

    Add pagination and landscape element details UI

--- ui/src/main/java/com/jeanbarrossilva/period/ui/composable/ChemicalElementDetailsUI.kt
+++ ui/src/main/java/com/jeanbarrossilva/period/ui/composable/ChemicalElementDetailsUI.kt
@@ -8,33 +8,81 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.recyclerview.widget.ItemTouchHelper
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
 import com.jeanbarrossilva.period.extensions.number.dp
 import com.jeanbarrossilva.period.extensions.view.updatePadding
-import com.jeanbarrossilva.period.model.ChemicalElement
+import com.jeanbarrossilva.period.model.ChemicalElementSortingOption
 import com.jeanbarrossilva.period.ui.composable.component.ChemicalElementHeadline
 import com.jeanbarrossilva.period.ui.composable.component.core.Landscape
 import com.jeanbarrossilva.period.ui.composable.component.core.Portrait
 import com.jeanbarrossilva.period.ui.composable.theme.PeriodTheme
 import com.jeanbarrossilva.period.ui.view.ChemicalElementDetailsView
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
-fun ChemicalElementDetailsUI(element: ChemicalElement, modifier: Modifier = Modifier) {
+fun ChemicalElementDetailsUI(elementIndex: Int, sortingOption: ChemicalElementSortingOption<*>, modifier: Modifier = Modifier) {
+    val elements = sortingOption.getElements()
+    val pagerState = rememberPagerState(pageCount = elements.size, initialPage = elementIndex)
+
     PeriodTheme {
-        Portrait {
-            LazyColumn(
-                Modifier
-                    .fillMaxSize()
-            ) {
-                item {
-                    Column(
-                        modifier
+        HorizontalPager(pagerState) { pageIndex ->
+            elements[pageIndex].let @Composable { element ->
+                Portrait {
+                    LazyColumn(
+                        Modifier
                             .fillMaxSize()
-                            .padding(top = 50.dp),
-                        Arrangement.spacedBy(50.dp)
+                    ) {
+                        item {
+                            Column(
+                                modifier
+                                    .fillMaxSize()
+                                    .padding(top = 50.dp),
+                                Arrangement.spacedBy(50.dp)
+                            ) {
+                                Box(
+                                    Modifier
+                                        .fillMaxWidth(),
+                                    Alignment.Center
+                                ) {
+                                    ChemicalElementHeadline(element)
+                                }
+
+                                Box(
+                                    Modifier
+                                        .fillMaxSize(),
+                                    Alignment.Center
+                                ) {
+                                    AndroidView(
+                                        { context ->
+                                            ChemicalElementDetailsView(context)
+                                        },
+                                        Modifier
+                                            .fillMaxSize()
+                                    ) { view ->
+                                        view.element = element
+                                        view.clipToPadding = false
+                                        if (view.canScrollVertically(ItemTouchHelper.UP)) {
+                                            view.updatePadding(top = 20.dp(view.context), bottom = 20.dp(view.context))
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                Landscape {
+                    Row(
+                        Modifier
+                            .fillMaxSize()
                     ) {
                         Box(
                             Modifier
-                                .fillMaxWidth(),
+                                .fillMaxWidth(0.5f)
+                                .fillMaxHeight()
+                                .padding(20.dp),
                             Alignment.Center
                         ) {
                             ChemicalElementHeadline(element)
@@ -45,63 +93,22 @@ fun ChemicalElementDetailsUI(element: ChemicalElement, modifier: Modifier = Modi
                                 .fillMaxSize(),
                             Alignment.Center
                         ) {
-                            AndroidView(
-                                { context ->
-                                    ChemicalElementDetailsView(context)
-                                },
+                            LazyColumn(
                                 Modifier
-                                    .fillMaxSize()
-                            ) { view ->
-                                view.element = element
-                                view.clipToPadding = false
-                                if (view.canScrollVertically(ItemTouchHelper.UP)) {
-                                    view.updatePadding(top = 20.dp(view.context), bottom = 20.dp(view.context))
+                                    .fillMaxSize(),
+                                verticalArrangement = Arrangement.spacedBy(0.dp, Alignment.CenterVertically)
+                            ) {
+                                item {
+                                    AndroidView(
+                                        { context ->
+                                            ChemicalElementDetailsView(context)
+                                        },
+                                        Modifier
+                                            .fillMaxSize()
+                                    ) { view ->
+                                        view.element = element
+                                    }
                                 }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        Landscape {
-            Row(
-                Modifier
-                    .fillMaxSize()
-            ) {
-                Box(
-                    Modifier
-                        .fillMaxWidth(0.5f)
-                        .fillMaxHeight()
-                        .padding(20.dp),
-                    Alignment.Center
-                ) {
-                    ChemicalElementHeadline(element)
-                }
-
-                Box(
-                    Modifier
-                        .fillMaxSize()
-                        .padding(20.dp),
-                    Alignment.Center
-                ) {
-                    LazyColumn(
-                        Modifier
-                            .fillMaxSize(),
-                        verticalArrangement = Arrangement.spacedBy(0.dp, Alignment.CenterVertically)
-                    ) {
-                        item {
-                            AndroidView(
-                                { context ->
-                                    ChemicalElementDetailsView(context)
-                                },
-                                Modifier
-                                    .fillMaxSize()
-                            ) { view ->
-                                view.element = element
-                                view.clipToPadding = false
-                                if (view.canScrollVertically(ItemTouchHelper.UP))
-                                    view.updatePadding(top = 20.dp(view.context), bottom = 20.dp(view.context))
                             }
                         }
                     }
--- ui/src/main/java/com/jeanbarrossilva/period/ui/fragment/ChemicalElementDetailsFragment.kt
+++ ui/src/main/java/com/jeanbarrossilva/period/ui/fragment/ChemicalElementDetailsFragment.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.bottomappbar.BottomAppBar
 import com.jeanbarrossilva.period.extensions.activity.fab
 import com.jeanbarrossilva.period.extensions.activity.toolbar
+import com.jeanbarrossilva.period.model.ChemicalElementSortingOption
 import com.jeanbarrossilva.period.ui.composable.ChemicalElementDetailsUI
+import com.jeanbarrossilva.period.ui.viewmodel.ChemicalElementDetailsViewModel
 
 class ChemicalElementDetailsFragment: Fragment() {
+    private val viewModel by viewModels<ChemicalElementDetailsViewModel>()
     private val navArgs by navArgs<ChemicalElementDetailsFragmentArgs>()
     private val element by lazy {
         navArgs.element
@@ -27,10 +34,23 @@ class ChemicalElementDetailsFragment: Fragment() {
         activity?.fab?.hide()
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setFragmentResultListener("sortingOption") { _, bundle ->
+            viewModel.setSortingOption(bundle["sortingOption"] as ChemicalElementSortingOption<*>)
+        }
+    }
+
+    @Suppress("LocalVariableName")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                ChemicalElementDetailsUI(element)
+                val _sortingOption by viewModel.getSortingOption().observeAsState()
+                val elementIndex = _sortingOption?.getElements().orEmpty().indexOf(element)
+
+                _sortingOption?.let { sortingOption ->
+                    ChemicalElementDetailsUI(elementIndex, sortingOption)
+                }
             }
         }
     }
