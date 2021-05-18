1d93ff3af11650295c1b21889b87f8194e0a873f
Jean Silva <jeanbarrossilva@outlook.com>
2021-05-18 17:27:29 -0300

    Add Activity navController extension

--- extensions/src/main/java/com/jeanbarrossilva/period/extensions/activity/Activity.kt
+++ extensions/src/main/java/com/jeanbarrossilva/period/extensions/activity/Activity.kt
@@ -4,18 +4,19 @@ import android.app.Activity
 import android.widget.FrameLayout
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.children
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.findNavController
 import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.jeanbarrossilva.period.extensions.view.searchFor
 
 private val Activity.content
     get() = findViewById<FrameLayout>(android.R.id.content)
-
 val Activity.fab
     get() = view.searchFor<FloatingActionButton>()
-
+val Activity.navController
+    get() = view.searchFor<FragmentContainerView>()?.findNavController()
 val Activity.toolbar
     get() = view.searchFor<Toolbar>() ?: view.searchFor<BottomAppBar>()
-
 val Activity.view
     get() = content.children.first()
