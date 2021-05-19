714180d0f493fc54816335f0335f2e6084d04102
Jean Silva <jeanbarrossilva@outlook.com>
2021-05-19 17:53:29 -0300

    Reimplement ChemicalElementSymbolView with Compose

--- ui/src/main/java/com/jeanbarrossilva/period/ui/compose/workaround/Workaround.kt
+++ ui/src/main/java/com/jeanbarrossilva/period/ui/compose/workaround/Workaround.kt
@@ -1,9 +1,20 @@
 package com.jeanbarrossilva.period.ui.compose.workaround
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
+import com.jeanbarrossilva.period.ui.R
+
+@Composable
+/** As of 1.0.0-beta-6, the default background color used in [Card] is white regardless of the current theme. **/
+fun cardBackgroundColor(): Color {
+    val value = ContextCompat.getColor(LocalContext.current, R.color.cardViewBackground)
+    return Color(value)
+}
 
 @Composable
 /** As of 1.0.0-beta06, the default color used in [Text] is black regardless of the current theme.  **/
